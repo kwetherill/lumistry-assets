0d9ba35399e681535af991ae8ad5a42bdbba31e7
Kirk Wetherill <megwetherill@gmail.com>
2025-09-26 17:43:55 -0500

    update

--- src/library/components/Form.jsx
+++ src/library/components/Form.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import '../styles.scss';
 
 function Form() {
-  return <h5>Try the most useless application!</h5>;
+  return <h5 className="lds-form">Try the most useless application!</h5>;
 }
 
 export default Form;
